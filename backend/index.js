bfb962bd505b9ab34d3a57adceac9ac65caf6e01
Lauri Koskenniemi <koskenniemilauri@gmail.com>
2021-11-09 00:25:15 +0200

    Removed test data

--- backend/index.js
+++ backend/index.js
@@ -6,40 +6,6 @@ const cors = require('cors')
 
 app.use(cors())
 
-const todo_lists = [
-  {
-    name: "school",
-    todos: [
-      {
-        id: 2,
-        deadline: "2021-11-10 22:00",
-        name: "assignment",
-        description: "",
-        status: "done!"
-      },
-      {
-        id: 3,
-        deadline: "2021-12-08 22:00",
-        name: "final assignment",
-        description: "Start this early!",
-        status: "not done"
-      }
-    ]
-  },
-  {
-    name: "work",
-    todos: [
-      {
-        id: 1,
-        deadline: "2021",
-        name: "This App",
-        description: "Finish this app and put it to Internet.",
-        status: "not done"
-      }
-    ]
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<p>try: /api/todos</p>')
 })
