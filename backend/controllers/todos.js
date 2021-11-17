e4ef0f042467823a2080f8ef8fb01205009327d1
Lauri Koskenniemi <koskenniemilauri@gmail.com>
2021-11-17 20:55:10 +0200

    Updated controller for new db models

--- backend/controllers/todos.js
+++ backend/controllers/todos.js
@@ -1,96 +1,165 @@
 const todosRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
-const TodoList = require('../models/todolist')
+const Todo = require('../models/todo')
+const List = require('../models/list')
 
-const getTokenFrom = (request) => {
+const getUserId = (request) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
+    const token = authorization.substring(7)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (decodedToken.id) {
+      return decodedToken.id
+    }
   }
+
   return null
 }
 
 todosRouter.get('/', async (request, response) => {
-  const token = getTokenFrom(request)
-  const decodedToken = token ? jwt.verify(token, process.env.SECRET) : {}
-  if (!token || !decodedToken.id) {
+  const userId = getUserId(request)
+  if (!userId) {
     return response.status(401).json({
       error: 'no valid token'
     })
   }
 
-  const lists = await TodoList.findOne({ user: decodedToken.id }).populate('user')
+  const lists = await List.find({ user: userId }).populate('todos')
   response.json(lists)
 })
 
-todosRouter.put('/', async (request, response) => {
-  const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+todosRouter.get('/lists', async (request, response) => {
+  const userId = getUserId(request)
+  if (!userId) {
     return response.status(401).json({
       error: 'no valid token'
     })
   }
 
-  const userLists = await TodoList.findOne({ user: decodedToken.id })
-  userLists.lists = userLists.lists.concat(body)
-  const savedLists = await userLists.save()
-  response.json(savedLists.lists)
+  const lists = await List.find({ user: userId })
+  response.json(lists)
 })
 
-todosRouter.put('/:list', async (request, response) => {
-  const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+todosRouter.get('/:list/todos', async (request, response) => {
+  const listId = request.params.list
+  const userId = getUserId(request)
+  if (!userId) {
     return response.status(401).json({
       error: 'no valid token'
     })
   }
 
-  const userLists = await TodoList.findOne({ user: decodedToken.id })
-  userLists.lists = userLists.lists.map(
-    list => list._id.toString() === request.params.list
-      ? { _id: list._id, name: list.name, todos: list.todos.concat(body) }
-      : list
-  )
-  const savedLists = await userLists.save()
-  response.json(savedLists.lists)
+  const todos = await Todo.find({ user: userId, list: listId })
+  response.json(todos)
+})
+
+todosRouter.get('/:todo', async (request, response) => {
+  const todoId = request.params.todo
+  const userId = getUserId(request)
+  if (!userId) {
+    return response.status(401).json({
+      error: 'no valid token'
+    })
+  }
+
+  const todo = await Todo.find({ user: userId, id: todoId })
+  response.json(todo)
+})
+
+todosRouter.post('/', async (request, response) => {
+  const body = request.body
+  const userId = getUserId(request)
+  if (!userId) {
+    return response.status(401).json({
+      error: 'no valid token'
+    })
+  }
+
+  const list = new List({
+    name: body.name,
+    todos: [],
+    user: userId
+  })
+
+  const savedList = await list.save()
+  response.json(savedList.toJSON())
+})
+
+todosRouter.post('/:list', async (request, response) => {
+  const body = request.body
+  const listId = request.params.list
+  const userId = getUserId(request)
+  if (!userId) {
+    return response.status(401).json({
+      error: 'no valid token'
+    })
+  }
+
+  const list = await List.findOne({ _id: listId })
+
+  const todo = new Todo({
+    deadline: new Date(body.deadline),
+    name: body.name,
+    description: body.description,
+    status: body.status || false,
+    user: userId,
+    list: list._id
+  })
+
+  const savedTodo = await todo.save()
+  list.todos = list.todos.concat(savedTodo._id)
+  await list.save()
+
+  response.json(savedTodo.toJSON())
+})
+
+todosRouter.put('/:todo', async (request, response) => {
+  const body = request.body
+  const todoId = request.params.todo
+  const userId = getUserId(request)
+  if (!userId) {
+    return response.status(401).json({
+      error: 'no valid token'
+    })
+  }
+
+  const todo = {
+    ...body
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(todoId, todo, { new: true })
+  response.json(updatedTodo)
 })
 
 todosRouter.delete('/:list', async (request, response) => {
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const listId = request.params.list
+  const userId = getUserId(request)
+  if (!userId) {
     return response.status(401).json({
       error: 'no valid token'
     })
   }
 
-  const userLists = await TodoList.findOne({ user: decodedToken.id })
-  userLists.lists = userLists.lists.filter(list => list._id.toString() !== request.params.list)
-  const savedLists = await userLists.save()
-  response.json(savedLists.lists)
+  await Todo.deleteMany({ list: listId })
+  await List.findByIdAndDelete(listId)
+  response.status(204).end()
 })
 
 todosRouter.delete('/:list/:todo', async (request, response) => {
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const todoId = request.params.todo
+  const userId = getUserId(request)
+  if (!userId) {
     return response.status(401).json({
       error: 'no valid token'
     })
   }
 
-  const userLists = await TodoList.findOne({ user: decodedToken.id })
-  userLists.lists = userLists.lists.map(
-    list => list._id.toString() === request.params.list
-      ? { _id: list._id, name: list.name, todos: list.todos.filter(todo => todo._id.toString() !== request.params.todo) }
-      : list
-  )
-  const savedLists = await userLists.save()
-  response.json(savedLists.lists)
+  const list = await List.findOne({ todos: todoId })
+  list.todos = list.todos.filter(todo => todo._id.toString() !== todoId)
+  await list.save()
+
+  await Todo.findByIdAndDelete(todoId)
+  response.status(204).end()
 })
 
 module.exports = todosRouter
